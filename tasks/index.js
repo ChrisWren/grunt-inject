a36871135e66ccea96a9b7d0796906e6a796e436
ChrisWren <cthewren@gmail.com>
2013-11-10 17:07:17 -0800

    Use the grunt file configuration to allow for dynamic src/dest html files

--- tasks/index.js
+++ tasks/index.js
@@ -2,30 +2,20 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.registerMultiTask('inject', 'Injects JavaScript into a page for development.', function () {
-    var htmlText;
+    var scriptSrc = this.data.scriptSrc;
     var scriptText;
 
-    if (this.data.htmlSrc) {
-      htmlText = grunt.file.read(this.data.htmlSrc);
-    } else {
-      grunt.log.error('Please specify an htmlSrc file that contains a <--inject--> where you want the script injected.');
-      return;
-    }
-
-    if (this.data.scriptSrc) {
-      scriptText = grunt.file.read(this.data.scriptSrc);
+    if (scriptSrc) {
+      scriptText = grunt.file.read(scriptSrc);
     } else {
       grunt.log.error('Please specify a scriptSrc file to inject into the html.');
       return;
     }
 
-    if (this.data.htmlDest) {
-      grunt.file.write(this.data.htmlDest, htmlText.replace('<!--inject-->','<script type="text/javascript">' + scriptText + '</script>'));
-      grunt.log.ok(this.data.scriptSrc + ' injected'.blue + ' into ' + this.data.htmlDest);
-    } else {
-      grunt.log.error('Please specify a htmlDest file where the injected html file should be output.');
-      return;
-    }
+    this.files.forEach(function (file) {
+      grunt.file.write(file.dest, grunt.file.read(file.src).replace('<!-- inject -->', '<script type="text/javascript">' + scriptText + '</script>'));
+      grunt.log.ok(scriptSrc + ' injected'.blue + ' into ' + file.dest);
+    });
 
   });
 };
